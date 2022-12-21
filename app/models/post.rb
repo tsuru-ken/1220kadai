8e9679307e168ed68d1f01b63b5db30fa48cb5af
takeshi tsuruda <lazywayofcat@gmail.com>
2022-12-21 12:29:36 +0900

    コミットメッセージ

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ApplicationRecord
-  belongs_to :user
   validates :image, presence: true
   validates :content, presence: true
+
   mount_uploader :image, ImageUploader
-  
+
+  belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
 end
